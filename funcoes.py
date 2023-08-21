b526cb51fe0ac8c56117ccf6107b60e627ebf03e
João Guilherme Mendanha Alves <alvesmega20@gmail.com>
2023-08-21 02:34:58 -0300

    Finalizando interface e algumas funcionalidades úteis

--- funcoes.py
+++ funcoes.py
@@ -1,4 +1,50 @@
-def mostrar_selecionados(checkbox_vars):
+import matplotlib.pyplot as mp
+
+def mostrar_selecionados(checkbox_vars, partidos, ano,treshold):
+    partidos_selecionados = []
     for i, var in enumerate(checkbox_vars):
-        selecionado = var.get()  # Obtém o estado da checkbox usando get()
-        print(f"Ano {i+2003}: {'Selecionado' if selecionado else 'Não selecionado'}")
+        selecionado = var.get()
+        if selecionado:
+            partidos_selecionados.append(partidos[i])
+    normalizacao(partidos_selecionados, ano,treshold)
+
+def normalizacao(partidos_selecionados, ano,treshold):
+
+    grafo_analise = {}
+    arquivo_politician = "dataset/politicians" + ano + ".txt"
+    arquivo_grafo = "dataset/graph" + ano + ".txt"
+
+    with open(arquivo_politician, 'r', encoding='utf-8') as file:
+        candidatos = []
+        grafo_qnt_votos = {}
+        for linha  in file:
+            elementos = linha.strip().split(';')
+            if elementos[1] in partidos_selecionados:
+                candidatos.append(elementos[0])
+                grafo_qnt_votos[elementos[0]] = int(elementos[2])
+
+    with open(arquivo_grafo, 'r', encoding='utf-8') as file:
+        for linha  in file:
+            elementos = linha.strip().split(';')
+            if elementos[0] in candidatos  and elementos[1] in candidatos:
+                peso = int(elementos[2])/min(grafo_qnt_votos[elementos[0]], grafo_qnt_votos[elementos[1]])
+                if peso >= float(treshold):
+                    grafo_analise[elementos[0]] = {}
+                    grafo_analise[elementos[0]][elementos[1]] = 1 - peso
+    print(grafo_analise)
+
+def grafico():
+    fig, ax = mp.subplots()
+
+    fruits = ['apple', 'blueberry', 'cherry', 'orange']
+    counts = [40, 100, 30, 55]
+    bar_labels = ['red', 'blue', '_red', 'orange']
+    bar_colors = ['tab:red', 'tab:blue', 'tab:red', 'tab:orange']
+
+    ax.bar(fruits, counts, label=bar_labels, color=bar_colors)
+
+    ax.set_ylabel('fruit supply')
+    ax.set_title('Fruit supply by kind and color')
+    ax.legend(title='Fruit color')
+
+    mp.show()
--- main.py
+++ main.py
@@ -2,58 +2,91 @@ import tkinter as tk
 from tkinter import ttk
 import funcoes as f
 
-def abrir_nova_tela():
+def abrir_nova_tela(checkbox_vars, partidos,s,treshold):
     root.withdraw()  # Esconde a janela principal
     nova_tela = tk.Toplevel(root)
     nova_tela.title("Nova Tela")
     
-    mensagem = "Bem-vindo à nova tela!"
+    valor_digitado = treshold.get()
+    mensagem = f"Resultados considerando um Treshold de {valor_digitado}"
     label = tk.Label(nova_tela, text=mensagem)
     label.pack(padx=20, pady=20)
+    label.configure(font=("Helvetica", 14))
 
     nova_tela.geometry(f"1200x700")
     nova_tela.geometry(f"+400-200")
+
+    f.mostrar_selecionados(checkbox_vars, partidos,s,valor_digitado)
     
     # Definindo uma função para fechar a nova tela e mostrar a janela principal novamente
     def fechar_nova_tela():
         nova_tela.destroy()  # Destroi a nova tela
         root.deiconify()  # Mostra a janela principal novamente
 
-    # Criando um botão na nova tela para fechar a nova tela e voltar à janela principal
     voltar_botao = tk.Button(nova_tela, text="Voltar", command=fechar_nova_tela)
     voltar_botao.place(x = 550, y = 600)
+    voltar_botao.configure(font=("Helvetica", 14))
 
 def selecionar_partidos():
-    mensagem = '''Bem-vindo a comparação de dados de Deputados Federais e suas votações
+    root.withdraw()  # Esconde a janela principal
+    nova_tela = tk.Toplevel(root)
+    nova_tela.title("Nova Tela")
+    nova_tela.geometry(f"900x500")
+    nova_tela.geometry(f"+500-300")
 
-    Selecione os partidos que deseja fazer as comparações'''
+    mensagem = '''Selecione os partidos que deseja fazer as comparações'''
 
-    label = tk.Label(root, text=mensagem)
-    label.place(x=120, y=20)
+    label = tk.Label(nova_tela, text=mensagem)
+    label.place(x=220, y=20)
     label.configure(font=("Helvetica", 14))
-    itens = ["2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011",
-        "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020",
-        "2021", "2022", "2023"]
-    
+
+    selecionado = tree.selection()
+    s = tree.item(selecionado, "text")
+
+    arquivo_ano = "dataset/politicians" + str(s) + ".txt"
+
+    with open(arquivo_ano, 'r', encoding='utf-8') as file:
+        partidos =[]
+        for linha in file:
+            elementos = linha.strip().split(';')
+            if elementos[1] not in partidos:
+                partidos.append(elementos[1])
+
     checkbox_vars = []
 
-    frame = tk.Frame(root) 
-    frame.place(x=440, y= 240, anchor="center") 
+    frame = tk.Frame(nova_tela) 
+    frame.place(x=440, y= 200, anchor="center") 
 
-    for i, item in enumerate(itens):
+    for i, partido in enumerate(partidos):
         linha = i // 3
         coluna = i % 3
         var = tk.BooleanVar(value=False)
         checkbox_vars.append(var)
-        checkbox = tk.Checkbutton(frame, text=item, variable=var)
+        checkbox = tk.Checkbutton(frame, text=partido, variable=var)
         checkbox.configure(font=("Helvetica", 14))
         checkbox.grid(row=linha, column=coluna, sticky="w")
+    
+    digite = tk.Label(nova_tela,text="Digite o Threshold (0.0 - 1.0):")
+    digite.pack()
+    digite.configure(font=("Helvetica", 14))
+    digite.place(x = 250, y = 350)
+    entrada = tk.Entry(nova_tela,width=5, )  # Defina o tamanho da entrada usando o parâmetro 'width'
+    entrada.pack()
+    entrada.place(x = 500, y = 350)
+    entrada.configure(font=("Helvetica", 14))
 
-    btn_mostrar = tk.Button(root, text="Mostrar resultados", command=lambda:  f.mostrar_selecionados(checkbox_vars))
+    btn_mostrar = tk.Button(nova_tela, text="Mostrar resultados", command=lambda:  abrir_nova_tela(checkbox_vars, partidos,s,entrada))
     btn_mostrar.pack()
     btn_mostrar.place(x=350, y=400)
     btn_mostrar.configure(font=("Helvetica", 14))
 
+    def fechar_nova_tela():
+        nova_tela.destroy()  # Destroi a nova tela
+        root.deiconify()  # Mostra a janela principal novamente
+
+    voltar_botao = tk.Button(nova_tela, text="Voltar", command=fechar_nova_tela)
+    voltar_botao.place(x = 400, y = 450)
+    voltar_botao.configure(font=("Helvetica", 14))
 
 root = tk.Tk()
 root.title("Comparação de dados de deputados e suas votações - BEM-VINDO !")
@@ -73,9 +106,9 @@ itens = ["2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011",
     "2021", "2022", "2023"]
 
 frame = tk.Frame(root)
-frame.place(x=330, y=130)
+frame.place(x=330, y=140)
 
-tree = ttk.Treeview(frame, selectmode="extended", height=9)
+tree = ttk.Treeview(frame, selectmode="extended", height=8)
 scrollbar = ttk.Scrollbar(frame, orient="vertical", command=tree.yview)
 
 tree["columns"] = ("item",)
